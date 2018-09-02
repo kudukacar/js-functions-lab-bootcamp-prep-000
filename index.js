d580e6f308b0021104fa161b7d08555bbc566bfb
Manju Thomas <kudukacar@hotmail.com>
2018-09-02 00:13:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 function happyHolidaysTo(Name) { 
-  return 'Happy holidays, ${Name}!';
+  return "Happy holidays," + name + "!";
 }
 function happyHolidayTo(Holiday, Name) {
-  return 'Happy ${Holiday}, ${Name}!';
+  return "Happy " + Holiday + " " + Name + "!";
 }
 function holidayCountdown(Days,Holiday) {
-  return 'It\'s ${Days} days until ${Holiday}!';
+  return "It\'s " + Days + " days until " + Holiday + "!";
 }
